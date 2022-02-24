958d27e1c4b5c05360888b51b74be29458697c99
alvesma2017 <alvesma@gmail.com>
2022-02-24 08:48:34 -0300

    Criando Rotas

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ const express = require("express")
 //constante "app" que inicializa o express
 const app = express()
 
+//criação da rota, enviando uma resposta em texto
+//se tentar enviar outro send na mesma rota dará erro
+app.get("/",function(req,res){
+
+res.send("bem vindo")
+
+})
+
+//criação da rota blog, enviando uma resposta em texto
+app.get("/blog",function(req,res){
+
+    res.send("bem vindo ao blog")
+    
+    })
+
+//criação da rota canal, enviando uma resposta em texto
+app.get("/canal",function(req,res){
+
+    res.send("bem vindo ao canal")
+    
+    })
+
 //chamando o servidor na porta 3000
 app.listen(3000,function(erro){
 
