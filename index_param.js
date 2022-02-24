7afad9d20be4f85a0d39cbfc431b77d3ed5817f8
alvesma2017 <alvesma@gmail.com>
2022-02-24 10:28:10 -0300

    Rota Parametrizada

--- index_param.js
+++ index_param.js
@@ -17,14 +17,27 @@ app.get("/blog",function(req,res){
 
     res.send("bem vindo ao blog")
     
-    })
+})
 
 //criação da rota canal, enviando uma resposta em texto
 app.get("/canal",function(req,res){
 
     res.send("bem vindo ao canal")
     
-    })
+})
+
+
+//criação da rota canal, enviando uma resposta em texto
+app.get("/ola/:nome/:empresa",function(req,res){
+
+    //retorna o parametros passado na URL
+    //res.send(req.params.nome)
+
+    var nome = req.params.nome
+    var empresa = req.params.empresa
+    res.send("<h1>Oi" + " "  + nome + " " + "da" + " "  + empresa + " </h1>")
+    
+})
 
 //chamando o servidor na porta 3000
 app.listen(3000,function(erro){
